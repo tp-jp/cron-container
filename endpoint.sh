c911153c7f18a45e793f060025e1b68688fe819d
tp.jp <chronokreuz@gmail.com>
2025-03-23 18:31:18 +0900

    設定の再読み込みを追加

--- endpoint.sh
+++ endpoint.sh
@@ -3,16 +3,11 @@ set -e
 touch /var/log/cron.log
 chmod 666 /var/log/cron.log
 
-#echo '* * * * * root echo "Cron job executed" >> /var/log/cron.log 2>&1' > cronjobs
-#echo '' >> cronjobs  # 最後に改行がないと cron が動かないことがある
-#chmod 644 cronjobs
-#chown root:root cronjobs
-#mv cronjobs /etc/cron.d/cronjobs
-ls -lah /etc/cron.d
-cat /etc/cron.d/cronjobs
-
 # cronをバックグラウンドで実行
 cron
+
+# cronの設定を再読み込み
+crontab /etc/cron.d/cronjobs
 
 # cronが動いている間はコンテナを保持
 tail -f /var/log/cron.log
