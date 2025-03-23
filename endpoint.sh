b8aba46b80e78bca6c50c2879a7287480bb98e06
tp.jp <chronokreuz@gmail.com>
2025-03-24 02:01:43 +0900

    全体設定に変更

--- endpoint.sh
+++ endpoint.sh
@@ -6,10 +6,10 @@ touch /var/log/cron.log
 chmod 666 /var/log/cron.log
 
 # /etc/cron.d 配下のすべての設定ファイルの権限を適正化
-#chmod 644 /etc/cron.d/*
-#chown root:root /etc/cron.d/*
-chmod 644 /var/spool/cron/crontabs/*
-chown root:root /var/spool/cron/crontabs/*
+chmod 644 /etc/cron.d/cronjobs
+chown root:root /etc/cron.d/cronjobs
+#chmod 644 /var/spool/cron/crontabs/*
+#chown root:root /var/spool/cron/crontabs/*
 
 # cronをバックグラウンドで実行
 echo "startup cron..." >> /var/log/cron.log
