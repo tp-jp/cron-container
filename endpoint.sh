c86ad8436589cf0f49e253c99bb2d6a036ea1e75
tp.jp <chronokreuz@gmail.com>
2025-03-23 18:25:02 +0900

    外部読み込みに変更

--- endpoint.sh
+++ endpoint.sh
@@ -3,11 +3,13 @@ set -e
 touch /var/log/cron.log
 chmod 666 /var/log/cron.log
 
-echo '* * * * * root echo "Cron job executed" >> /var/log/cron.log 2>&1' > cronjobs
-echo '' >> cronjobs  # 最後に改行がないと cron が動かないことがある
-chmod 644 cronjobs
-chown root:root cronjobs
-mv cronjobs /etc/cron.d/cronjobs
+#echo '* * * * * root echo "Cron job executed" >> /var/log/cron.log 2>&1' > cronjobs
+#echo '' >> cronjobs  # 最後に改行がないと cron が動かないことがある
+#chmod 644 cronjobs
+#chown root:root cronjobs
+#mv cronjobs /etc/cron.d/cronjobs
+ls -lah /etc/cron.d
+cat /etc/cron.d/cronjobs
 
 # cronをバックグラウンドで実行
 cron
