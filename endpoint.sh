0056d4fa7ca0d911ccd8185bf5c9ddb50b784fa5
tp.jp <chronokreuz@gmail.com>
2025-03-23 23:43:31 +0900

    不要なテストコードを削除

--- endpoint.sh
+++ endpoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
+
+# cronログを作成
 touch /var/log/cron.log
 chmod 666 /var/log/cron.log
 
-chmod 644 /etc/cron.d/cronjobs
-chown root:root /etc/cron.d/cronjobs
+# /etc/cron.d 配下のすべての設定ファイルの権限を適正化
+chmod 644 /etc/cron.d/*
+chown root:root /etc/cron.d/*
 
 # cronをバックグラウンドで実行
 echo "startup cron..." >> /var/log/cron.log
